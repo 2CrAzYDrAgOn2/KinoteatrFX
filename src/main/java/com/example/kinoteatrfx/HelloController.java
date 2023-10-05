9c7f8bbbc0010e22fa77bf04188af06d29b477d0
2CrAzYDrAgOn2 <cdkaw258@mail.ru>
2023-10-05 23:25:51 +0300

    v1.0

--- src/main/java/com/example/kinoteatrfx/HelloController.java
+++ src/main/java/com/example/kinoteatrfx/HelloController.java
@@ -1,17 +1,13 @@
 package com.example.kinoteatrfx;
 
-import java.net.URL;
-import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 
 public class HelloController {
-    @FXML
-    private GridPane gridPane;
 
-    private boolean[][] isGreen = new boolean[7][10];
+    private final boolean[][] isRed = new boolean[7][10];
 
 
     @FXML
@@ -19,12 +15,12 @@ public class HelloController {
         Button btn = (Button)event.getSource();
         int rowIndex = GridPane.getRowIndex(btn) == null ? 0 : GridPane.getRowIndex(btn);
         int columnIndex = GridPane.getColumnIndex(btn) == null ? 0 : GridPane.getColumnIndex(btn);
-        if (isGreen[rowIndex][columnIndex]){
-            isGreen[rowIndex][columnIndex] = false;
-            btn.setStyle("-fx-background-color: #ff0000");
+        if (isRed[rowIndex][columnIndex]){
+            isRed[rowIndex][columnIndex] = false;
+            btn.setStyle("-fx-background-color: green");
         } else {
-            isGreen[rowIndex][columnIndex] = true;
-            btn.setStyle("-fx-background-color: #fa0123");
+            isRed[rowIndex][columnIndex] = true;
+            btn.setStyle("-fx-background-color: red");
         }
     }
 
